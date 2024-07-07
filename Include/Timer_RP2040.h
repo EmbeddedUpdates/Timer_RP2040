7cfe5ddaa04c0f6c070b65994e594befe8810be3
Patrick Flaherty <patrickdanielflaherty@gmail.com>
2024-07-07 14:00:13 -0400

    handwritten first draft

--- Include/Timer_RP2040.h
+++ Include/Timer_RP2040.h
@@ -1,15 +1,15 @@
 
 /**
- * @file 
+ * @file Timer_RP2040.h
  * @author pflaherty
- * @date 
- * @brief 
+ * @date 06.24.2024
+ * @brief Main Header file for the RP2040 Timer module
  * 
- * COMPONENT: 
- * VERSION: 
+ * COMPONENT: TIMER_RP2040
+ * VERSION: 01.00.00
  *
  * Change History
- * 
+ * 01.00.00 - Initial Creation - pflaherty
  */
 
 
@@ -20,9 +20,18 @@
 /************************************************************
   INCLUDES
 ************************************************************/
-#Include "Timer_RP2040_SFR.h"
+#include "Platform_Types.h"
+#include "Timer_RP2040_SFR.h"
 /************************************************************
   ENUMS AND TYPEDEFS
+************************************************************/
+
+/************************************************************
+  GLOBAL VARIABLES
+************************************************************/
+
+/************************************************************
+  LOCAL VARIABLES
 ************************************************************/
 
 /************************************************************
@@ -33,3 +42,182 @@
   GLOBAL FUNCTIONS
 ************************************************************/
 
+/**
+ * BRIEF: Initializes the Timer module, and arms Alarm0
+ * DESC: Enables the timer
+ *        1. Checks the timer is was init previously
+ *        2. Pauses the Timer
+ *        3. Resets the Timer
+ *        4. Disarms all alarms
+ *        5. Resets globabl variables for tracking
+ * 
+ * RETURN: 
+ *         0: 'OK' if the value can be stored
+ *         1: 'NOT_OK' if the timer write fails
+ * 
+ * PRECONDITIONS: Timer is not Enabled
+ * POSTCONDITIONS: Timer is enabled
+ * INVARIANTS: None
+ * 
+ */
+Std_ErrorCode Timer_RP2040_Init ( void );
+
+/**
+ * BRIEF: Pauses the Timer, Resets the Timer, Disarms all alarms.
+ * DESC: Shuts down the Timer so that it can be restarted.
+ *        1. Checks the timer is was init previously
+ *        2. Pauses the Timer
+ *        3. Resets the Timer
+ *        4. Disarms all alarms
+ *        5. Resets globabl variables for tracking
+ * 
+ * RETURN: 0 if Successful, other for specific failure
+ * 
+ * PRECONDITIONS: Timer is enabled
+ * POSTCONDITIONS: Timer is not enabled
+ * INVARIANTS: None
+ * 
+ */
+Std_ReturnType Timer_RP2040_Deinit ( void );
+
+
+/**
+ * FUNC: Std_ReturnType Function_Name ( void )
+ * BRIEF: Short Description about the function
+ * DESC: Longer description of how it works
+ * 
+ * RETURN: 0 if Successful, other for specific failure
+ * 
+ * PRECONDITIONS: Timer is Initialized
+ * POSTCONDITIONS: Timer is Paused
+ * INVARIANTS: None
+ * 
+ */
+Std_ReturnType Timer_RP2040_InterruptEnable ( void );
+
+/**
+ * FUNC: Std_ReturnType Function_Name ( void )
+ * BRIEF: Short Description about the function
+ * DESC: Longer description of how it works
+ * 
+ * RETURN: 0 if Successful, other for specific failure
+ * 
+ * PRECONDITIONS: Timer is Initialized
+ * POSTCONDITIONS: Timer is Paused
+ * INVARIANTS: None
+ * 
+ */
+Std_ReturnType Timer_RP2040_InterruptDisable ( void );
+
+/**
+ * FUNC: Std_ReturnType Function_Name ( void )
+ * BRIEF: Short Description about the function
+ * DESC: Longer description of how it works
+ * 
+ * RETURN: 0 if Successful, other for specific failure
+ * 
+ * PRECONDITIONS: Timer is Initialized
+ * POSTCONDITIONS: Timer is Paused
+ * INVARIANTS: None
+ * 
+ */
+Std_ReturnType Timer_RP2040_ReadTimer ( uint32 * TimerHigh, uint32 * TimerLow );
+
+/**
+ * FUNC: Std_ReturnType Function_Name ( void )
+ * BRIEF: Short Description about the function
+ * DESC: Longer description of how it works
+ * 
+ * RETURN: 0 if Successful, other for specific failure
+ * 
+ * PRECONDITIONS: Timer is Initialized
+ * POSTCONDITIONS: Timer is Paused
+ * INVARIANTS: None
+ * 
+ */
+Std_ReturnType Timer_RP2040_SetTimer ( uint32 * TimerHigh, uint32 * TimerLow );
+
+/**
+ * FUNC: Std_ReturnType Function_Name ( void )
+ * BRIEF: Short Description about the function
+ * DESC: Longer description of how it works
+ * 
+ * RETURN: 0 if Successful, other for specific failure
+ * 
+ * PRECONDITIONS: Timer is Initialized
+ * POSTCONDITIONS: Timer is Paused
+ * INVARIANTS: None
+ * 
+ */
+Std_ReturnType Timer_RP2040_ReadTimerLow ( uint32 * TimerLow );
+
+/**
+ * FUNC: Std_ReturnType Function_Name ( void )
+ * BRIEF: Short Description about the function
+ * DESC: Longer description of how it works
+ * 
+ * RETURN: 0 if Successful, other for specific failure
+ * 
+ * PRECONDITIONS: Timer is Initialized
+ * POSTCONDITIONS: Timer is Paused
+ * INVARIANTS: None
+ * 
+ */
+Std_ReturnType Timer_RP2040_ReadTimerHigh ( uint32 * TimerHigh );
+
+/**
+ * FUNC: Std_ReturnType Function_Name ( void )
+ * BRIEF: Short Description about the function
+ * DESC: Longer description of how it works
+ * 
+ * RETURN: 0 if Successful, other for specific failure
+ * 
+ * PRECONDITIONS: Timer is Initialized
+ * POSTCONDITIONS: Timer is Paused
+ * INVARIANTS: None
+ * 
+ */
+Std_ReturnType Timer_RP2040_SetTimerLow ( uint32 * TimerLow );
+
+/**
+ * FUNC: Std_ReturnType Function_Name ( void )
+ * BRIEF: Short Description about the function
+ * DESC: Longer description of how it works
+ * 
+ * RETURN: 0 if Successful, other for specific failure
+ * 
+ * PRECONDITIONS: Timer is Initialized
+ * POSTCONDITIONS: Timer is Paused
+ * INVARIANTS: None
+ * 
+ */
+Std_ReturnType Timer_RP2040_SetTimerHigh ( uint32 * TimerHigh );
+
+
+/**
+ * FUNC: Std_ReturnType Function_Name ( void )
+ * BRIEF: Short Description about the function
+ * DESC: Longer description of how it works
+ * 
+ * RETURN: 0 if Successful, other for specific failure
+ * 
+ * PRECONDITIONS: Timer is Initialized
+ * POSTCONDITIONS: Timer is Paused
+ * INVARIANTS: None
+ * 
+ */
+Std_ReturnType Timer_RP2040_CheckAlarm ( uint8 n );
+
+/**
+ * FUNC: Std_ReturnType Function_Name ( void )
+ * BRIEF: Short Description about the function
+ * DESC: Longer description of how it works
+ * 
+ * RETURN: 0 if Successful, other for specific failure
+ * 
+ * PRECONDITIONS: Timer is Initialized
+ * POSTCONDITIONS: Timer is Paused
+ * INVARIANTS: None
+ * 
+ */
+Std_ReturnType Timer_RP2040_DisarmAlarm ( uint8 n );
--- Source/Timer_RP2040.c
+++ Source/Timer_RP2040.c
@@ -21,6 +21,7 @@
   INCLUDES
 ************************************************************/
 #include "Timer_RP2040.h"
+
 /************************************************************
   ENUMS AND TYPEDEFS
 ************************************************************/
@@ -30,6 +31,219 @@
 ************************************************************/
 
 /************************************************************
+  LOCAL FUNCTIONS
+************************************************************/
+
+/**
+ * BRIEF: Pauses the RP2040 Timer
+ * DESC: Writes to the TIMER_PAUSE register, reports OK is Timer is paused.
+ *       Reports NOT_OK if the timer did not pause
+ * 
+ * RETURN: 
+ *         0: 'OK' if the Timer becomes paused
+ *         1: 'NOT_OK' if the timer is not paused
+ *         3: 'MODULE_UNINIT' if the timer is not yet initialized.
+ * 
+ * PRECONDITIONS: Timer is enabled. Timer is not paused.
+ * POSTCONDITIONS: Timer is paused. 
+ * INVARIANTS: None
+ * 
+ */
+static Std_ErrorCode Timer_RP2040_Pause ( void )
+{
+}
+
+/**
+ * BRIEF: Unpauses the RP2040 Timer
+ * DESC: Writes to the TIMER_PAUSE register, reports OK is Timer is unpaused.
+ *       Reports NOT_OK if the timer did not pause
+ * 
+ * RETURN: 
+ *         0: 'OK' if the Timer becomes paused
+ *         1: 'NOT_OK' if the timer is not paused
+ *         3: 'MODULE_UNINIT' if the timer is not yet initialized.
+ * 
+ * PRECONDITIONS: Timer is enabled. Timer is paused.
+ * POSTCONDITIONS: Timer is running.
+ * INVARIANTS: None
+ * 
+ */
+static Std_ErrorCode Timer_RP2040_Unpause( void )
+{
+
+}
+
+/**
+ * BRIEF: Read lower word of the timer register
+ * DESC: Reads from TIMER_TIMELR. Reports bits {31:0} of the timer value and stores it in TimerLow param.
+ * 
+ * PARAM: TimerLow - buffer to store the value of the timer
+ * 
+ * RETURN: 
+ *         0: 'OK' if the Timer can be read
+ *         1: 'NOT_OK' if the timer read fails
+ *         2: 'INVALID_PARAM' if the provided buffer is null
+ *         3: 'MODULE_UNINIT' if the timer is not yet initialized.
+ * 
+ * PRECONDITIONS: None
+ * POSTCONDITIONS: None
+ * INVARIANTS: None
+ * 
+ */
+static Std_ErrorCode Timer_RP2040_ReadTimerLow ( uint32 * TimerLow )
+{
+
+}
+
+/**
+ * BRIEF: Read Upper word from the timer. Stores to provided buffer.
+ * DESC: Reads from TIMER_TIMERAWH. Stores bits {63:32} to the provided buffer.
+ * 
+ * PARAM: TimerHigh - buffer to store the value of the timer
+ * 
+ * RETURN: 
+ *         0: 'OK' if the Timer can be read
+ *         1: 'NOT_OK' if the timer read fails
+ *         2: 'INVALID_PARAM' if the provided buffer is null
+ *         3: 'MODULE_UNINIT' if the timer is not yet initialized.
+ * 
+ * PRECONDITIONS: None
+ * POSTCONDITIONS: None
+ * INVARIANTS: None
+ * 
+ */
+static Std_ErrorCode Timer_RP2040_ReadTimerHigh ( uint32 * TimerHigh )
+{
+
+}
+
+/**
+ * BRIEF: Stores the provided value to the lower 32 bits of the timer
+ * DESC: Stores to TIMER_TIMELW
+ * 
+ * PARAM: TimerLow - buffer to store the value of the timer
+ * 
+ * RETURN: 
+ *         0: 'OK' if the value can be stored
+ *         1: 'NOT_OK' if the timer write fails
+ *         2: 'INVALID_PARAM' if the provided buffer is null
+ *         3: 'MODULE_UNINIT' if the timer is not yet initialized.
+ * 
+ * PRECONDITIONS: None
+ * POSTCONDITIONS: None
+ * INVARIANTS: None
+ * 
+ */
+static Std_ErrorCode Timer_RP2040_SetTimerLow ( uint32 * TimerLow )
+{
+
+}
+
+/**
+ * BRIEF: Stores the provided value to the higher 32 bits of the timer
+ * DESC: Stores to TIMER_TIMEHW
+ * 
+ * PARAM: TimerHigh - buffer to store the value of the timer
+ * 
+ * RETURN: 
+ *         0: 'OK' if the value can be stored
+ *         1: 'NOT_OK' if the timer write fails
+ *         2: 'INVALID_PARAM' if the provided buffer is null
+ *         3: 'MODULE_UNINIT' if the timer is not yet initialized.
+ * 
+ * PRECONDITIONS: None
+ * POSTCONDITIONS: None
+ * INVARIANTS: None
+ * 
+ */
+static Std_ErrorCode Timer_RP2040_SetTimerHigh ( uint32 * TimerHigh )
+{
+
+}
+
+
+/************************************************************
   GLOBAL FUNCTIONS
 ************************************************************/
+
+/**
+ * Initializes the Timer module, and arms Alarm0
+ */
+Std_ErrorCode Timer_RP2040_Init ( void )
+{
+  Std_ErrorCode retVal = NOT_OK;
+  return retVal;
+}
+
+/**
+ * Pauses the Timer, Resets the Timer, Disarms all alarms.
+ */
+Std_ErrorCode Timer_RP2040_Deinit ( void )
+{
+  Std_ErrorCode retVal = NOT_OK;
+  return retVal;
+}
+
+/**
+ * Enables Interrupts for the timer - Should be used with care
+ */
+Std_ErrorCode Timer_RP2040_InterruptEnable ( void )
+{
+  Std_ErrorCode retVal = NOT_OK;
+  return retVal;
+}
+
+/**
+ * Disables Interrupts for the timer - Should be used with care
+ */
+Std_ErrorCode Timer_RP2040_InterruptDisable ( void )
+{
+  Std_ErrorCode retVal = NOT_OK;
+  return retVal;
+}
+
+/**
+ * Stores the current timer value into the two provided uint32 buffers
+ */
+Std_ErrorCode Timer_RP2040_ReadTimer ( uint32 * TimerHigh, uint32 * TimerLow )
+{
+  Std_ErrorCode retVal = NOT_OK;
+  return retVal;
+}
+
+/**
+ * Loads the provided uint32 buffers into the Timer counter
+ */
+Std_ErrorCode Timer_RP2040_SetTimer ( uint32 * TimerHigh, uint32 * TimerLow )
+{
+  Std_ErrorCode retVal = NOT_OK;
+  return retVal;
+}
+
+/**
+ * Reports 'True' if the alarm has been triggered, False if not
+ */
+Std_ErrorCode Timer_RP2040_CheckAlarm ( uint8 n )
+{
+  Std_ErrorCode retVal = NOT_OK;
+  return retVal;
+}
+
+/**
+ * Enables the Alarm
+ */
+Std_ErrorCode Timer_RP2040_SetAlarm ( uint8 n )
+{
+  Std_ErrorCode retVal = NOT_OK;
+  return retVal;
+}
+
+/**
+ * Disarms the alarm
+ */
+Std_ErrorCode Timer_RP2040_DisarmAlarm ( uint8 n )
+{
+  Std_ErrorCode retVal = NOT_OK;
+  return retVal;
+}
 
